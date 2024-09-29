2d7ae11da18b215241d1a8ea75e0ead4f9906f6f
Davidcasstella <brayan.lopez07@uptc.edu.co>
2024-09-28 23:19:08 -0500

    mirando si funcina

--- loquesea.py
+++ loquesea.py
@@ -1,2 +1,3 @@
 print ("Lo que sea para mirar como hacer por lo menos una rama principal en github")
 print("Hola Mundo")
+print ("bueno nose porque no quedo el primer mensaje david del futuro pero son los mismo comandos que para java")
